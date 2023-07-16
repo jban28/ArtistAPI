fa55fa8d0da0a1963f379a3979e146bb46c7aced
James <jd.bannister28@gmail.com>
2023-07-16 15:51:41 +0100

    add all-images-by-series route

--- deployment/application.py
+++ deployment/application.py
@@ -72,6 +72,23 @@ def all_images():
         image_list_return.append(image)
     return image_list_return
 
+@application.route("/all-images-by-series")
+def all_images_by_series():
+    image_collection = db[request.args.get("artist")]
+    image_list_return = {}
+    image_list = image_collection.find().sort("sequence", -1)
+    for image in image_list:
+        image["_id"] = str(image["_id"])
+        image["srcFull"] = f"{root_URL}/{image['srcFull']}"
+        image["srcThumb"] = f"{root_URL}/{image['srcThumb']}"
+        
+        if image["series"] in image_list_return:
+            image_list_return[image["series"]].append(image)
+        else:
+            image_list_return[image["series"]] = [image]
+
+    return image_list_return
+
 @application.route("/image", methods=["POST"])
 @user_only
 def new_image_file(artist_name):
@@ -179,6 +196,5 @@ def reorder_images(artist_name):
     return "complete"
 
 if __name__ == "__main__":
-    application.debug = True
     application.run()
 
