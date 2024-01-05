8fb02cd4a02100651c53e583e9eb00d7806f8d34
jban28 <jd.bannister28@gmail.com>
2024-01-05 10:11:19 +0000

    fix yml error in deployment action

--- lambda-functions/all-images.py
+++ lambda-functions/all-images.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implemen
+    # TODO implement
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
