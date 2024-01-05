7f45df509ea2ffec4b7b5570f55c08f5b77e4112
jban28 <jd.bannister28@gmail.com>
2024-01-05 13:08:15 +0000

    test deployment to lambda

--- .github/scripts/changed-files.sh
+++ .github/scripts/changed-files.sh
@@ -10,7 +10,7 @@ do
 done
 
 num="${#changed_func_files[@]}"
-echo $num
+echo $changed_func_files
 
 echo "files_to_deploy=${changed_func_files[@]}" >> $GITHUB_OUTPUT
 echo "changes=${num}" >> $GITHUB_OUTPUT
--- lambda-functions/login.py
+++ lambda-functions/login.py
@@ -1,7 +1,7 @@
 import json
 
 def lambda_handler(event, context):
-    # TODO implement testing again and again and again and again and again
+    # TODO implement testing again and again and again and again
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
