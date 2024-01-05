3a0e22264f082bbf65ecff082cc0b1618b9d971a
jban28 <jd.bannister28@gmail.com>
2024-01-05 10:57:35 +0000

    test deployment to lambda

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,10 @@
+FILES_TO_DEPLOY=("lambda-functions/login.py")
+
+for file in "${FILES_TO_DEPLOY[@]}"
+do
+    zip -j lambda.zip $file
+    str="${file%.py}"
+    func_name="ArtistAPI_${str#*/}"
+    echo $func_name
+    # aws lambda update-function-code --function-name=ArtistAPI_"${file%.py}" --zip-file=fileb://lambdaAPI.zip
+done
