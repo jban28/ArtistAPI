a545b4d48b67f75174115c8fcf98b4e72ff03348
jban28 <jd.bannister28@gmail.com>
2024-01-05 11:50:54 +0000

    test deployment to lambda

--- .github/scripts/changed-files.sh
+++ .github/scripts/changed-files.sh
@@ -12,4 +12,4 @@ num="${#changed_func_files[@]}"
 echo $num
 
 echo "files_to_deploy=${changed_func_files[@]}" >> $GITHUB_OUTPUT
-echo "changes={num}" >> $GITHUB_OUTPUT
+echo "changes=${num}" >> $GITHUB_OUTPUT
