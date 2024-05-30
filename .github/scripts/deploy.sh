4c828ef1be7d4ea5f9072536d7a0f12846e6bb3c
jban28 <91541400+jban28@users.noreply.github.com>
2024-05-30 09:19:05 +0100

    testing github action

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -1,6 +1,7 @@
 for func in "${FUNCS_TO_DEPLOY[@]}"
 do
     zip -j lambda.zip $func
+    echo $func
     func_name="ArtistAPI_${func}"
     echo $func_name
     aws lambda update-function-code --function-name="${func_name}" --zip-file=fileb://lambda.zip
