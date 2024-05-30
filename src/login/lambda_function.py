97f1d0d4f6f352bb19f3f7f1dd849e035d15666e
jban28 <91541400+jban28@users.noreply.github.com>
2024-05-30 09:51:01 +0100

    test action with 2 funcs modified

--- src/login/lambda_function.py
+++ src/login/lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda but hopefully a final test!')
+        'body': json.dumps('Hello from Lambda but hopefully another final test!')
     }
