57e5bc1e8b4e2a11868134501b217701ff2bfab4
jban28 <91541400+jban28@users.noreply.github.com>
2024-05-30 09:46:21 +0100

    recursive zip

--- .github/scripts/deploy.sh
+++ .github/scripts/deploy.sh
@@ -6,7 +6,7 @@ read -ra funcs_to_deploy <<<"${FUNCS_TO_DEPLOY[@]}"
 for func in "${funcs_to_deploy}"
 do
     file_path="src/${func}"
-    zip -j lambda.zip $file_path
+    zip -r -j lambda.zip $file_path
     echo $func
     func_name="ArtistAPI_${func}"
     echo $func_name
