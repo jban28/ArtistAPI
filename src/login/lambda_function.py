cd1684f8cab4684ff02c52ca6e0ad861318d30c1
jban28 <91541400+jban28@users.noreply.github.com>
2024-05-30 11:32:57 +0100

    add login function

--- src/login/lambda_function.py
+++ src/login/lambda_function.py
@@ -1,8 +1,39 @@
 import json
+import os
+import pymongo
+import sha256
+import jwt
+
+secret = os.environ["secretKey"]
+root_URL = os.environ["rootURL"]
+db_uri = os.environ["databaseURI"]
+
+dbclient = pymongo.MongoClient(db_uri, server_api=pymongo.server_api.ServerApi('1'))
+db = dbclient["artCollections"]
 
 def lambda_handler(event, context):
     # TODO implement
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda but a test 4!')
+    auth = event["headers"]["Authorization"].split(":")
+    username = auth[0]
+    password = auth[1]
+    try:
+        user = db["users"].find_one({"username" : username},{"_id":0})
+        if sha256(password.encode('utf-8')).hexdigest() == user["password"]:
+            token = jwt.encode(
+                {
+                "artist": user["name"],
+                }, secret, algorithm="HS256")
+            return {
+                'statusCode': 200,
+                'body': json.dumps(token)
+            }
+        else:
+            return {
+                'statusCode': 401,
+                'body': json.dumps('Password does not match username')
+            }
+    except:
+        return {
+        'statusCode': 401,
+        'body': json.dumps('Username not found')
     }
