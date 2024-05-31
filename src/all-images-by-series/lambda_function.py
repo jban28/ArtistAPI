b0fca781763a53118c1e3c98158667ab6c37a5b6
jban28 <91541400+jban28@users.noreply.github.com>
2024-05-31 15:38:22 +0100

    add all-images-by-series

---
+++ src/all-images-by-series/lambda_function.py
@@ -0,0 +1,31 @@
+import json
+import os
+import pymongo
+
+secret = os.environ["secretKey"]
+root_URL = os.environ["rootURL"]
+db_uri = os.environ["databaseURI"]
+
+dbclient = pymongo.MongoClient(db_uri, server_api=pymongo.server_api.ServerApi('1'))
+db = dbclient["artCollections"]
+
+
+def lambda_hander(event, context):
+    artist = event["queryStringParameters"]["artist"]
+
+
+def all_images_by_series(artist):
+    image_collection = db['artist']
+    image_list_return = {}
+    image_list = image_collection.find().sort("sequence", -1)
+    for image in image_list:
+        image["_id"] = str(image["_id"])
+        image["srcFull"] = f"{root_URL}/{image['srcFull']}"
+        image["srcThumb"] = f"{root_URL}/{image['srcThumb']}"
+        
+        if image["series"] in image_list_return:
+            image_list_return[image["series"]].append(image)
+        else:
+            image_list_return[image["series"]] = [image]
+
+    return image_list_return
