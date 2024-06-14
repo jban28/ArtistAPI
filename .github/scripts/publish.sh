bcff5cd3f83223288a5bc5dde134b58f86975258
jban28 <91541400+jban28@users.noreply.github.com>
2024-06-14 10:53:30 +0100

    Update publish.sh

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -5,5 +5,5 @@ for func_file in "${lambda_funcs[@]}"; do
     func_name="ArtistAPI_${func_name##*/}"
 
     version=$(aws lambda publish-version --function-name="${func_name}" --query "Version")
-    aws lambda update-alias --function-name "${func_name}" --name "live" --function-version "$version"
+    aws lambda update-alias --function-name "${func_name}" --name "live" --function-version $version
 done
